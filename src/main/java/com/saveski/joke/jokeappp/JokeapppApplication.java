d0d54e7a4ef25d2253c2768c72539e403c334c35
ssaveski <saveskistef@gmai.l.com>
2020-03-31 18:11:38 +0200

    Initial Commit

---
+++ src/main/java/com/saveski/joke/jokeappp/JokeapppApplication.java
@@ -0,0 +1,13 @@
+package com.saveski.joke.jokeappp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeapppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeapppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saveski/joke/jokeappp/JokeapppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saveski.joke.jokeappp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeapppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
