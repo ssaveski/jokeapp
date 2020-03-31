fb855f887324c19f6d18fb448129099506b1c821
ssaveski <saveskistef@gmai.l.com>
2020-03-31 22:23:05 +0200

    Refactoring

---
+++ src/main/java/com/saveski/joke/jokeappp/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package com.saveski.joke.jokeappp.controllers;
+
+import com.saveski.joke.jokeappp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
---
+++ src/main/java/com/saveski/joke/jokeappp/services/JokeService.java
@@ -0,0 +1,7 @@
+package com.saveski.joke.jokeappp.services;
+
+public interface JokeService {
+
+    String getJoke();
+
+}
